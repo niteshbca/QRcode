b4e7df80c41c53e680d409ac1526a9a18645ecd1
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-01-05 14:24:18 +0530

    Update History.js

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -8,7 +8,7 @@ const History = () => {
     // Fetch sales data from the backend
     const fetchSalesData = async () => {
       try {
-        const response = await fetch('http://54.94.34.169:5000/api/sales');
+        const response = await fetch('http://18.229.150.112:5000/api/sales');
         const data = await response.json();
         setSalesData(data);
       } catch (error) {
