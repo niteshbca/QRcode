d6ca2ed3ffa675d4069673a34aa69a6a92d4f210
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-01-06 06:27:07 +0530

    Update AdminLogin.js

--- frontend/src/components/AdminLogin.js
+++ frontend/src/components/AdminLogin.js
@@ -12,7 +12,7 @@ const AdminLogin = () => {
     e.preventDefault();
 
     // Use the API URL from the .env file
-    const apiUrl = "http://18.231.120.244:5000";
+    const apiUrl = "http://15.228.204.180:5000";
 
     // API call to backend to check credentials
     try {
